309cf4f1170845dbb8d0ff5f7f6ba19bdefd5c4d
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-10-08 23:22:00 -0500

    Update CriptoHeader color

--- src/components/CriptoHeader.js
+++ src/components/CriptoHeader.js
@@ -1,47 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import styled from "styled-components";
-import BASEURL from "../api/criptosStats";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import BASEURL from '../api/criptosStats';
 
 const CriptoHeader = (criptosInfo) => {
-    const [topCripto, setTopCripto] = useState();
-    const crypto = criptosInfo.criptosInfo
+  const [topCripto, setTopCripto] = useState([]);
+  const crypto = criptosInfo.criptosInfo;
 
-    useEffect(() => {
-        const criptoInfo = async () => {
-            const info = await BASEURL.get("/coins/markets", {
-                params: {
-                    vs_currency: "eur",
-                    ids: crypto.join(),
-                }
-            })
-            setTopCripto(info.data);
-        }
-        criptoInfo()
-    });
+  useEffect(() => {
+    const criptoInfo = async () => {
+      const info = await BASEURL.get('/coins/markets', {
+        params: {
+          vs_currency: 'eur',
+          ids: crypto.join(),
+        },
+      });
+      setTopCripto(info.data);
+    };
+    criptoInfo();
+  }, [crypto]);
 
-    function currencyFormat(current_price) {
-        return  parseFloat(current_price).toFixed(2).replace(/(?=(\d{3})+(?!\d))/g, '.' ) + ' €'
-      }
-
+  function currencyFormat(current_price) {
     return (
-        <Block>
-            {topCripto ?
-                topCripto.map((cripto) =>
-                    <>
-                        <p>{cripto.name} - {currencyFormat(cripto.current_price)} /</p>
-                    </>
-                )
-                : null}
-        </Block>
-    )
-}
+      parseFloat(current_price)
+        .toFixed(2)
+        .replace(/(?=(\d{3})+(?!\d))/g, '.') + ' €'
+    );
+  }
 
-const Block = styled.div`
-    display: flex;
-    height: 30px;
-    justify-content: space-between;
-    background: #dad6d6;
-    z-index: 10;
+  return (
+    <TopBar>
+      <Container>
+        {topCripto.length > 0 &&
+          topCripto.map((cripto) => (
+            <strong key={cripto.id}>
+              {cripto.name} - {currencyFormat(cripto.current_price)}
+            </strong>
+          ))}
+      </Container>
+    </TopBar>
+  );
+};
+
+const TopBar = styled.div`
+  background: #466280;
+  padding: 0.5rem;
 `;
 
-export default CriptoHeader
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+  max-width: 80rem;
+  margin: 0 auto;
+  text-transform: uppercase;
+`;
+
+export default CriptoHeader;
